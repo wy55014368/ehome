2764613a7eebc29a066d932c832ce5c08c996507
yandong123123 <1425865132@qq.com>
2017-01-22 20:18:34 +0800

    Revert "Revert "将三层结构（之前的内容）注释起来，方便修改，有需要就可以添加方法""
    This reverts commit bcb290df13469a9003ed9b1047ed2d59f1dbd747.

--- src/com/ehome/util/UserHandler.java
+++ src/com/ehome/util/UserHandler.java
@@ -1,14 +1,8 @@
 package com.ehome.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.ehome.entity.User;
-
-
 public class UserHandler {
 
-	private List<User> list = new ArrayList<User>();
+	/*private List<User> list = new ArrayList<User>();
 	
 	public void addUser(User user){
 		list.add(user);
@@ -16,5 +10,5 @@ public class UserHandler {
 	
 	public List<User> select(){
 		return list;
-	}
+	}*/
 }
